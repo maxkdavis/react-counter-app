ef6e55f116590290ae747d1cdb8c5461b6cfb947
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2025-06-25 11:35:14 -0400

    added counter steps functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,34 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [counterSteps, setCounterSteps] = useState(1);
 
   function incrementCount() {
-    setCounter((prevCount) => prevCount + 1);
+    setCounter((prevCount) => prevCount + counterSteps);
   }
 
   function decrementCount() {
-    setCounter((prevCount) => prevCount - 1);
+    setCounter((prevCount) => prevCount - counterSteps);
   }
 
   function resetCount() {
     setCounter(0);
   }
 
+  function handleChange(e) {
+    // console.log(e.target.value);
+    if (e.target.value === '') {
+      setCounterSteps(1);
+      return;
+    }
+    setCounterSteps(+e.target.value); //'+' is a shorthand to convert the input value into a number
+  }
+
   return (
     <>
       <div className='counter-container'>
         <h1>React Counter App</h1>
+
         <span className='counter'>{counter}</span>
         <div className='btn-container'>
           <button onClick={incrementCount}>
@@ -32,6 +43,14 @@ function App() {
             <ion-icon name='refresh-circle-outline'></ion-icon>
           </button>
         </div>
+        <form>
+          <input
+            className='counterStepsInput'
+            type='number'
+            placeholder='Set counter steps'
+            onChange={handleChange}
+          ></input>
+        </form>
       </div>
     </>
   );
