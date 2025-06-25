939f4bfcf9566fb061db34ec29d569694c179213
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2025-06-24 22:15:21 -0400

    counter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,31 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCount() {
+    setCounter((prevCount) => prevCount + 1);
+  }
+
+  function decrementCount() {
+    setCounter((prevCount) => prevCount - 1);
+  }
+
+  function resetCount() {
+    setCounter(0);
+  }
+
   return (
     <>
-      <h1>Hello from App Componenet</h1>
+      <div className='counter-container'>
+        <h1>React Counter App</h1>
+        <span className='counter'>{counter}</span>
+        <div className='btn-container'>
+          <button onClick={incrementCount}>+</button>
+          <button onClick={decrementCount}>-</button>
+          <button onClick={resetCount}>reset</button>
+        </div>
+      </div>
     </>
   );
 }
